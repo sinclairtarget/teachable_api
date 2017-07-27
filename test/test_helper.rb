4bd4c8c8fa7dfb25a3200aa10b859eeb147a52ed
Sinclair Target <sinclairtarget@gmail.com>
2017-07-27 16:19:26 -0400

    Configure VCR gem.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,8 @@
 require 'minitest/autorun'
+require 'vcr'
 require 'teachable_api'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/vcr_cassettes'
+  config.hook_into :webmock
+end
